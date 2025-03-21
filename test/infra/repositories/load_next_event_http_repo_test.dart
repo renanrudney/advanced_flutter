1a32df63aa809e0241106a1f64742beb386e0e3c
Renan Rudney <renanrudney@gmail.com>
2025-03-20 21:14:30 -0300

    ensure LoadNextEventHttpRepository throws UnexpectedError on 500

--- test/infra/repositories/load_next_event_http_repo_test.dart
+++ test/infra/repositories/load_next_event_http_repo_test.dart
@@ -33,6 +33,8 @@ class LoadNextEventHttpRepository implements LoadNextEventRepository {
       throw DomainError.unexpected;
     } else if (response.statusCode == 404) {
       throw DomainError.unexpected;
+    } else if (response.statusCode == 500) {
+      throw DomainError.unexpected;
     }
     final event = jsonDecode(response.body);
     return NextEvent(
@@ -194,6 +196,12 @@ void main() {
 
   test('should throw UnexpectedError on 404', () async {
     httpClient.statusCode = 404;
+    final future = sut.loadNextEvent(groupId: groupId);
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('should throw UnexpectedError on 500', () async {
+    httpClient.statusCode = 500;
     final future = sut.loadNextEvent(groupId: groupId);
     expect(future, throwsA(DomainError.unexpected));
   });
