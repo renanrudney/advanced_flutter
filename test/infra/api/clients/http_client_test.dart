300091e3121cf41c879dc87135d0eaaaa1213e12
Renan Rudney <renanrudney@gmail.com>
2025-03-21 21:38:51 -0300

    ensure HttpClient throws UnexpectedError on error

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -107,5 +107,24 @@ void main() {
       final future = sut.get(url: url);
       expect(future, throwsA(DomainError.sessionExpired));
     });
+
+
+    test('should throw UnexpectedError on 403', () async {
+      client.simulateForbiddenError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw UnexpectedError on 404', () async {
+      client.simulateNotFoundError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
+
+    test('should throw UnexpectedError on 500', () async {
+      client.simulateServerError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
+    });
   });
 }
