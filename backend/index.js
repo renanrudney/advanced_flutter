338b094435473255005407a74c668dc2547df3db
Renan Rudney <renanrudney@gmail.com>
2025-04-07 20:33:56 -0300

    add factories

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/api/groups/:groupId/next_event', (req, res) => {
+  if (req.params['groupId'] !== 'valid_id') return res.status(400).send(Error('invalid id'))
   res.send({
     "id": "1",
     "groupName": "Pelada Chega+",
