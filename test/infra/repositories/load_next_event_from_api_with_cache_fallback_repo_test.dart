9aadea35aef7f1c072ac497ad836c0ecaa72daf6
Renan Rudney <renanrudney@gmail.com>
2025-04-14 20:43:22 -0300

    ensure LoadNextEventFromApiWithCacheFallbackRepository load data from api

---
+++ test/infra/repositories/load_next_event_from_api_with_cache_fallback_repo_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../mocks/fakes.dart';
+
+final class LoadNextEventFromApiWithCacheFallbackRepository {
+  final Future<void> Function({ required String groupId }) loadNextEventFromApi;
+
+  const LoadNextEventFromApiWithCacheFallbackRepository({
+    required this.loadNextEventFromApi,
+  });
+
+  Future<void> loadNextEvent({ required String groupId }) async {
+    await loadNextEventFromApi(groupId: groupId);
+  }
+}
+
+final class LoadNextEventRepositorySpy {
+  String? groupId;
+  int callsCount = 0;
+
+  Future<void> loadNextEvent({ required String groupId }) async {
+    this.groupId = groupId;
+    callsCount++;
+  }
+}
+
+void main() {
+  late String groupId;
+  late LoadNextEventRepositorySpy apiRepo;
+  late LoadNextEventFromApiWithCacheFallbackRepository sut;
+
+  setUp(() {
+    groupId = anyString();
+    apiRepo = LoadNextEventRepositorySpy();
+    sut = LoadNextEventFromApiWithCacheFallbackRepository(
+      loadNextEventFromApi: apiRepo.loadNextEvent,
+    );
+  });
+
+  test('should load event data from api repo', () async {
+    await sut.loadNextEvent(groupId: groupId);
+    expect(apiRepo.groupId, groupId);
+    expect(apiRepo.callsCount, 1);
+  });
+}
