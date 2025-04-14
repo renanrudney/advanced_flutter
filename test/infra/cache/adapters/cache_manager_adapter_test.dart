352cd4a1ce297232ec69690d4185b53fc0589088
Renan Rudney <renanrudney@gmail.com>
2025-04-14 20:16:15 -0300

    ensure CacheManagerAdapter returns null if readAsString fails

--- test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -15,8 +15,8 @@ final class CacheManagerAdapter {
   Future<dynamic> get({ required String key }) async {
     final info = await client.getFileFromCache(key);
     if (info?.validTill.isBefore(DateTime.now()) != false || !await info!.file.exists()) return null;
-    final data = await info.file.readAsString();
     try {
+      final data = await info.file.readAsString();
       return jsonDecode(data);
     } catch (err) {
       return null;
@@ -29,8 +29,10 @@ final class FileSpy implements File {
   int readAsStringCallCount = 0;
   bool _fileExists = true;
   String _response = '{}';
+  Error? _readAsStringError;
 
   void simulateEmptyFile() => _fileExists = false;
+  void simulateReadAsStringError() => _readAsStringError = Error();
   void simulateInvalidResponse() => _response = 'invalid_json';
   void simulateResponse(String response) => _response = response;
 
@@ -43,6 +45,7 @@ final class FileSpy implements File {
   @override
   Future<String> readAsString({Encoding encoding = utf8}) async {
     readAsStringCallCount++;
+    if (_readAsStringError != null) throw _readAsStringError!;
     return _response;
   }
 
@@ -291,5 +294,11 @@ void main() {
     final json = await sut.get(key: key);
     expect(json['key1'], 'value1');
     expect(json['key1'], 'value1');
+  });
+
+  test('should return null if file.readAsString fails', () async {
+    client.file.simulateReadAsStringError();
+    final json = await sut.get(key: key);
+    expect(json, isNull);
   });
 }
