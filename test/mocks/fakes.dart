fd7beb90d0663eb963392e6b1e154bf312db0967
Renan Rudney <renanrudney@gmail.com>
2025-04-15 22:24:33 -0300

    change cache date from datetime to iso date

--- test/mocks/fakes.dart
+++ test/mocks/fakes.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 int anyInt() => Random().nextInt(999999999);
-String anyString() => anyInt.toString();
+String anyString() => anyInt().toString();
 bool anyBool() => Random().nextBool();
 DateTime anyDate() => DateTime.fromMillisecondsSinceEpoch(anyInt());
 String anyIsoDate() => DateTime.fromMillisecondsSinceEpoch(anyInt()).toIso8601String();
