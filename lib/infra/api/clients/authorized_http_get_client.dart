a8a11eb2bfcde900349cb3ce427e9d02a6a8d38d
Renan Rudney <renanrudney@gmail.com>
2025-04-20 17:54:56 -0300

    move AuthorizedHttpGetClient to its own file

---
+++ lib/infra/api/clients/authorized_http_get_client.dart
@@ -0,0 +1,20 @@
+import 'package:advanced_flutter/infra/api/clients/http_get_client.dart';
+import 'package:advanced_flutter/infra/cache/clients/cache_get_client.dart';
+import 'package:advanced_flutter/infra/types/json.dart';
+
+final class AuthorizedHttpGetClient implements HttpGetClient {
+  final CacheGetClient cacheClient;
+  final HttpGetClient httpClient;
+
+  const AuthorizedHttpGetClient({
+    required this.cacheClient,
+    required this.httpClient,
+  });
+
+  @override
+  Future<dynamic> get({ required String url, Json? headers, Json? params, Json? queryString }) async {
+    final user = await cacheClient.get(key: 'current_user');
+    if (user?['accessToken'] != null) headers = (headers ?? {})..addAll({ 'authorization': user['accessToken'] });
+    return httpClient.get(url: url, params: params, queryString: queryString, headers: headers);
+  }
+}
